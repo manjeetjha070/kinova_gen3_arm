03ea6037a21f0ecbbca396b6cfdccafb5b952469
manjeetjha070 <manjeetjha070@gmail.com>
2026-01-13 13:02:24 +0100

    first version of pick and place task

---
+++ pick_place/launch/pick_place_demo.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("kinova_gen3_7dof_robotiq_2f_85").to_dict()
+    #moveit_config = MoveItConfigsBuilder("moveit_resources_panda").to_dict()
+
+    # MTC Demo node
+    pick_place_demo = Node(
+        package="pick_place",
+        executable="mtc_node",
+        output="screen",
+        parameters=[
+            moveit_config,
+        ],
+    )
+
+    return LaunchDescription([pick_place_demo])
